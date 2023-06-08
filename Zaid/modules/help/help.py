803a1362599239a49f86a52de3f2227043dcf254
TOXIC-BOY-OP <134930342+TOXIC-BOY-OP@users.noreply.github.com>
2023-06-08 07:35:50 +0530

    Update help.py

--- Zaid/modules/help/help.py
+++ Zaid/modules/help/help.py
@@ -58,7 +58,7 @@ async def module_help(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @TheUpdatesChannel"
+            this_command += "© @HEROKU_CREDIT_CARD2"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @TheSupportChat × @TheUpdatesChannel •"
+            message, f"```{str(ac)}```\n• @DOSTO_KI_M3HFIL × @HEROKU_CREDIT_CARD2 •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
